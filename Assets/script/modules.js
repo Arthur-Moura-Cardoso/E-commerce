55172765a8d3abc6211451aeda8d3a79234fe85b
Arthur-Moura-Cardoso <arthurmoura.ti.2007@gmail.com>
2025-07-29 21:43:57 -0300

    Finalizar estilização e adicionar script para a section 'produtos similares'

---
+++ Assets/script/modules.js
@@ -0,0 +1,125 @@
+export let produtos = [
+    {
+        id: 1,
+        imagens: [
+            "https://ultragames.com.br/wp-content/uploads/elementor/thumbs/nintendo-switch2-post-r7d4phtw50udgwmr5nxwiyvbijjz6ptbe9np2hqy6g.png",
+            "https://cdn.awsli.com.br/2500x2500/2537/2537139/produto/240077027/vvcv-2rj8krlaf2.png",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w=",
+            "https://img.br.my-best.com/product_images/a8ef81a7ba109a63d1f1c9905bf04187.jpg?ixlib=rails-4.3.1&q=70&lossless=0&w=800&h=800&fit=clip&s=b51d3800170b22cf0a2571cd9a4e3d22",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w="
+        ],
+        nome: "Produto 1",
+        valor: 29.99,
+        descricao: "Produto básico ideal para uso diário com excelente custo-benefício."
+    },
+    {
+        id: 2,
+        imagens: [
+            "https://img.br.my-best.com/product_images/a8ef81a7ba109a63d1f1c9905bf04187.jpg?ixlib=rails-4.3.1&q=70&lossless=0&w=800&h=800&fit=clip&s=b51d3800170b22cf0a2571cd9a4e3d22",
+            "https://ultragames.com.br/wp-content/uploads/elementor/thumbs/nintendo-switch2-post-r7d4phtw50udgwmr5nxwiyvbijjz6ptbe9np2hqy6g.png",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w=",
+            "https://cdn.awsli.com.br/2500x2500/2537/2537139/produto/240077027/vvcv-2rj8krlaf2.png",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w="
+        ],
+        nome: "Produto 2",
+        valor: 39.99,
+        descricao: "Design moderno e funcionalidades essenciais para o seu dia a dia."
+    },
+    {
+        id: 3,
+        imagens: [
+            "https://cdn.awsli.com.br/2500x2500/2537/2537139/produto/240077027/vvcv-2rj8krlaf2.png",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w=",
+            "https://img.br.my-best.com/product_images/a8ef81a7ba109a63d1f1c9905bf04187.jpg?ixlib=rails-4.3.1&q=70&lossless=0&w=800&h=800&fit=clip&s=b51d3800170b22cf0a2571cd9a4e3d22",
+            "https://ultragames.com.br/wp-content/uploads/elementor/thumbs/nintendo-switch2-post-r7d4phtw50udgwmr5nxwiyvbijjz6ptbe9np2hqy6g.png",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w="
+        ],
+        nome: "Produto 3",
+        valor: 49.99,
+        descricao: "Alta durabilidade e desempenho superior por um preço acessível."
+    },
+    {
+        id: 4,
+        imagens: [
+            "https://ultragames.com.br/wp-content/uploads/elementor/thumbs/nintendo-switch2-post-r7d4phtw50udgwmr5nxwiyvbijjz6ptbe9np2hqy6g.png",
+            "https://cdn.awsli.com.br/2500x2500/2537/2537139/produto/240077027/vvcv-2rj8krlaf2.png",
+            "https://img.br.my-best.com/product_images/a8ef81a7ba109a63d1f1c9905bf04187.jpg?ixlib=rails-4.3.1&q=70&lossless=0&w=800&h=800&fit=clip&s=b51d3800170b22cf0a2571cd9a4e3d22",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w=",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w="
+        ],
+        nome: "Produto 4",
+        valor: 59.99,
+        descricao: "Perfeito para quem busca qualidade sem abrir mão da economia."
+    },
+    {
+        id: 5,
+        imagens: [
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w=",
+            "https://ultragames.com.br/wp-content/uploads/elementor/thumbs/nintendo-switch2-post-r7d4phtw50udgwmr5nxwiyvbijjz6ptbe9np2hqy6g.png",
+            "https://cdn.awsli.com.br/2500x2500/2537/2537139/produto/240077027/vvcv-2rj8krlaf2.png",
+            "https://img.br.my-best.com/product_images/a8ef81a7ba109a63d1f1c9905bf04187.jpg?ixlib=rails-4.3.1&q=70&lossless=0&w=800&h=800&fit=clip&s=b51d3800170b22cf0a2571cd9a4e3d22",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w="
+        ],
+        nome: "Produto 5",
+        valor: 69.99,
+        descricao: "Produto versátil com acabamento premium e tecnologia atualizada."
+    },
+    {
+        id: 6,
+        imagens: [
+            "https://cdn.awsli.com.br/2500x2500/2537/2537139/produto/240077027/vvcv-2rj8krlaf2.png",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w=",
+            "https://ultragames.com.br/wp-content/uploads/elementor/thumbs/nintendo-switch2-post-r7d4phtw50udgwmr5nxwiyvbijjz6ptbe9np2hqy6g.png",
+            "https://img.br.my-best.com/product_images/a8ef81a7ba109a63d1f1c9905bf04187.jpg?ixlib=rails-4.3.1&q=70&lossless=0&w=800&h=800&fit=clip&s=b51d3800170b22cf0a2571cd9a4e3d22",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w="
+        ],
+        nome: "Produto 6",
+        valor: 79.99,
+        descricao: "Potente e eficiente, atende até os usuários mais exigentes."
+    },
+    {
+        id: 7,
+        imagens: [
+            "https://ultragames.com.br/wp-content/uploads/elementor/thumbs/nintendo-switch2-post-r7d4phtw50udgwmr5nxwiyvbijjz6ptbe9np2hqy6g.png",
+            "https://img.br.my-best.com/product_images/a8ef81a7ba109a63d1f1c9905bf04187.jpg?ixlib=rails-4.3.1&q=70&lossless=0&w=800&h=800&fit=clip&s=b51d3800170b22cf0a2571cd9a4e3d22",
+            "https://cdn.awsli.com.br/2500x2500/2537/2537139/produto/240077027/vvcv-2rj8krlaf2.png",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w=",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w="
+        ],
+        nome: "Produto 7",
+        valor: 89.99,
+        descricao: "Combina alto desempenho com estilo elegante e moderno."
+    },
+    {
+        id: 8,
+        imagens: [
+            "https://img.br.my-best.com/product_images/a8ef81a7ba109a63d1f1c9905bf04187.jpg?ixlib=rails-4.3.1&q=70&lossless=0&w=800&h=800&fit=clip&s=b51d3800170b22cf0a2571cd9a4e3d22",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w=",
+            "https://ultragames.com.br/wp-content/uploads/elementor/thumbs/nintendo-switch2-post-r7d4phtw50udgwmr5nxwiyvbijjz6ptbe9np2hqy6g.png",
+            "https://cdn.awsli.com.br/2500x2500/2537/2537139/produto/240077027/vvcv-2rj8krlaf2.png",
+            "https://media.istockphoto.com/id/178716575/pt/foto/dispositivos-m%C3%B3veis.jpg?s=612x612&w=0&k=20&c=GXwN-2r9TZ2-do8-Wg4bBnoahNIpAIvSoO1Dtt69E7w="
+        ],
+        nome: "Produto 8",
+        valor: 99.99,
+        descricao: "Top de linha, com tecnologia de ponta e recursos avançados."
+    }
+];
+
+
+export function adicionarLinkProdutos (){
+    let cardProdutos = document.querySelectorAll(".card_produto")
+    
+    cardProdutos.forEach(card => {
+        card.addEventListener("click", () => {
+            let produtoId = card.getAttribute("meta-id") - 1
+            localStorage.setItem("produtoSelecionado", JSON.stringify({
+                id: produtos[produtoId].id,
+                imagens: produtos[produtoId].imagens,
+                nome: produtos[produtoId].nome,
+                valor: produtos[produtoId].valor,
+                descricao: produtos[produtoId].descricao
+            }))
+            window.location.href = "produto.html"
+        })
+    })
+}
--- Assets/script/produto.js
+++ Assets/script/produto.js
@@ -1,25 +1,90 @@
-let imagemPrincipalContainer = document.querySelector('.produto_img_principal');
-let imagensSegundariasContainer = document.querySelector('.produto_mini_imgs');
-let produtoNomeContainer = document.querySelector('.produto_nome');
-let produtoValorContainer = document.querySelector('.produto_valor');
-let produtoParceladoContainer = document.querySelector('.produto_parcelado_valor');
-let produtoDescricaoContainer = document.querySelector('.produto_descricao');
+import { produtos, adicionarLinkProdutos } from "./modules.js";
 
-let renderizarProduto = () => {
-    let produto = localStorage.getItem('produtoSelecionado');
-    produto = JSON.parse(produto);
-    let produtoNome = produto.nome;
-    let produtoValor = produto.valor;
-    let produtoImagem = produto.imagem;
-    let produtoDescricao = produto.descricao;
+function renderizarProduto() {
+    let produtoNomeContainer = document.querySelector('.produto_nome');
+    let produtoValorContainer = document.querySelector('.produto_valor');
+    let produtoParceladoContainer = document.querySelector('.produto_parcelado_valor');
+    let produtoDescricaoContainer = document.querySelector('.produto_descricao');
+    let produto = receberProdutos()
+    
+    produtoNomeContainer.textContent = produto.produtoNome;
+    produtoValorContainer.textContent = `R$${produto.produtoValor.toFixed(2)}`;
+    produtoParceladoContainer.textContent = `R$${produto.produtoValor.toFixed(2)} em até 12x de R$${(produto.produtoValor / 12).toFixed(2)}`;
+    produtoDescricaoContainer.textContent = produto.produtoDescricao;
 
-    let valorParcelado = produtoValor / 10;
+    adicionarimagens(produto.produtoImagem);
+    adicionarProdutoSimilares(produtos, produto.produtoId);
 
-    produtoNomeContainer.textContent = produtoNome;
-    produtoValorContainer.textContent = `R$${produtoValor.toFixed(2)}`;
-    produtoParceladoContainer.textContent = `R$${produtoValor.toFixed(2)} em até 12x de R$${(produtoValor / 12).toFixed(2)}`;
-    imagemPrincipalContainer.src = produtoImagem;
-    produtoDescricaoContainer.textContent = produtoDescricao;
 }
 
 renderizarProduto();
+
+function receberProdutos() {
+    let produto = localStorage.getItem('produtoSelecionado');
+    produto = JSON.parse(produto);
+    
+    let produtoId = produto.id;
+    let produtoNome = produto.nome;
+    let produtoValor = produto.valor;
+    let produtoImagem = produto.imagens;
+    let produtoDescricao = produto.descricao;
+
+    return {
+        produtoId,
+        produtoNome,
+        produtoValor,
+        produtoImagem,
+        produtoDescricao
+    };
+}
+    
+
+function adicionarimagens(imagens) {
+    let imagemPrincipalContainer = document.querySelector('.produto_img_principal');
+    let miniImagensContainer = document.querySelector('.produto_mini_imgs');
+
+    imagemPrincipalContainer.src = imagens[0];
+
+    for (let i = 0; i < miniImagensContainer.children.length; i++) {
+        miniImagensContainer.children[i].src = imagens[i]; 
+    }
+
+    let miniImagens = document.querySelectorAll('.mini_img');
+
+    miniImagens.forEach((miniImagem) => {
+        miniImagem.addEventListener('click', () => {
+            imagemPrincipalContainer.src = miniImagem.src;
+        });
+    });
+}
+
+function adicionarProdutoSimilares(produtos, idProdutoAtual) {
+    let produtosContainer = document.querySelector('.produtos_container');
+    let tamanhoContainer = 4;
+    
+    for (let i = 0; produtosContainer.children.length < tamanhoContainer; i++) {
+        let cardProduto = document.createElement('div');
+        cardProduto.classList.add('card_produto');
+        cardProduto.setAttribute('meta-id', produtos[i].id);
+        
+        if (produtos[i].id === idProdutoAtual) {
+            i++;
+        }
+    
+        cardProduto.innerHTML = `
+            <img class="card_produto_img" src="${produtos[i].imagens[0]}" alt="${produtos[i].nome}">
+            <div class="card_produto_flex">
+                <div class="card_produto_info">
+                    <h3 class="card_produto_nome">${produtos[i].nome}</h3>
+                    <img class="card_produto_classificacao" src="Assets/img/estrelas.png" alt="">
+                    <button class="card_produto_botao">Comprar</button>
+                </div>
+                <p class="card_produto_valor">R$${produtos[i].valor.toFixed(2)}</p>
+            </div>
+        `;
+
+        produtosContainer.appendChild(cardProduto);
+    };
+
+    adicionarLinkProdutos();
+}
